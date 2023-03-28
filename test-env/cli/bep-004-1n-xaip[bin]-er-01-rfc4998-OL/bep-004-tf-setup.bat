09840d46e3913e00dd80357e81725a074aaa5713
ervta <ervta@gmx.de>
2023-03-28 10:29:24 +0200

    test of -attributes

---
+++ test-env/cli/bep-004-1n-xaip[bin]-er-01-rfc4998-OL/bep-004-tf-setup.bat
@@ -0,0 +1,5 @@
+set CLABEL=[-] ein binäres Datenobjekt mit zugehörigen ER gem. RFC4998 (OL) (extra attributes and NOT SigningCertificateV2), geprüft gegen Basis-ERS-Profil
+set CPROF=BasisERProfile
+set AOID=2660725e-8eef-4645-986a-1a51af7e8265
+set CDATA=XAIP.xml
+set CER=erDataMSOS-001.ers
